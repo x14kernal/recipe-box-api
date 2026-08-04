088101b32a902be58baff14e5f421575b672a1ec
muhammed <muahmed932@gmail.com>
2026-08-04 11:59:32 +0300

    feat: add recipe creation, update, and validation

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,10 @@ import recipesRouter from './routes/recipe.js';
 
 const app: Express = express();
 
-app.use(express.json()); // to read data coming in from the client
+app.use(express.json()); // to read data coming in from the client, convert JSON into JS object
 app.get('/', (req, res) => {
   res.json({
-    message: 'Check http://localhost:3000/recipes',
+    message: 'Check http://localhost:3000/api/recipes',
   });
 });
 app.use('/api/recipes', recipesRouter);
