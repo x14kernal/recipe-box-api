c835b20750653717345d3883e55796f1d18065e5
muhammed <muahmed932@gmail.com>
2026-08-09 08:50:03 +0300

    feat: add CORS and request logging

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import 'dotenv/config';
 import express, { type Express } from 'express';
+import cors from 'cors';
 import recipesRouter from './routes/recipe.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import logger from './middlewares/logger.js';
 
 const app: Express = express();
-
+app.use(cors());
 app.use(express.json()); // to read data coming in from the client, convert JSON into JS object
+app.use(logger);
 
 app.get('/', (req, res) => {
   res.json({
