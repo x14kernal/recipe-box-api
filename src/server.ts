86038bd52a39db3770099423f92ef126dce04ed3
muhammed <muahmed932@gmail.com>
2026-08-16 09:10:47 +0300

    feat: set up Prisma with PostgreSQL

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
 import app from './app.js';
 
+// import { prisma } from './lib/prisma.js';
+
+// async function testDb() {
+//   await prisma.$connect();
+//   console.log('✅ Connected to Neon \n');
+
+//   const users = await prisma.users.findMany();
+//   console.log('\n Users:', users);
+
+//   await prisma.$disconnect();
+// }
+
+// testDb();
+
 const PORT = Number(process.env.PORT);
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(`Server running on ${PORT} \n`);
 });
